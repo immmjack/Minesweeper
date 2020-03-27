41dd835f0c2c6f896bff06f58452674aa878e1c0
Jack Yang <51064693+immmjack@users.noreply.github.com>
2020-03-27 13:45:23 +0800

    Add files via upload
    Start the game

---
+++ Play.java
@@ -0,0 +1,15 @@
+/**
+ * Filename: Play.java
+ * Author: Jack Yang
+ * Date: March 27, 2020
+ *
+ * This class is used to run the Minesweeper game.
+ */
+public class Play {
+
+    private static final String MINESWEEPER = "Minesweeper";
+
+    public static void main(String[] args) {
+        new Menu(MINESWEEPER);
+    }
+}
